076de84fc66a0bcf756b00b76f148d5abe3004da
josemenav <joseantoniomendozanavarro@gmail.com>
2023-08-07 20:06:18 -0600

    added shop page and some cart functionality

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,8 @@
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { Outlet, Link } from "react-router-dom"
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase.utils";
@@ -7,6 +10,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext); 
+  const {isCartOpen} = useContext(CartContext)
 
     return(
       <Fragment>
@@ -23,7 +27,9 @@ const Navigation = () => {
                 <Link className="nav-link" to='/auth'>Sign in</Link>
               )
             }
+            <CartIcon/>
           </div>
+          {isCartOpen &&  <CartDropdown/>}
         </div>
         <Outlet/>
       </Fragment>
